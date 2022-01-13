0b1192268bf5f596bd0bc32a1e13150bb33180bb
VitalyiKudo <vitalyipasechnyi@gmail.com>
2022-01-13 10:33:29 +0200

    add space to array

--- thirdTask.js
+++ thirdTask.js
@@ -23,7 +23,20 @@ let format = (array, formating, limit) => {
     });
   });
 
- 
+  const formatedArr = arr.map((str) => {
+    const isLeft = str.indexOf("*") === 0; // find the position of the star
+    if (str.length - 1 < limit) {
+      const empty = Array(limit - (str.length - 1)) // create array of space
+        .fill("\xa0")
+        .join("");
+      return isLeft ? `${str}${empty}*` : `*${empty}${str}`; // add array of space
+    } else {
+      return isLeft ? `${str}*` : `*${str}`;
+    }
+  });
+
+  formatedArr.push("*".repeat(limit + 2)); // top stars
+  formatedArr.unshift("*".repeat(limit + 2)); // bottom stars
 
   return arr
 };
