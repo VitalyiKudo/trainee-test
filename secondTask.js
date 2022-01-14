9b0c84540d70df09cebafc8022fd9e4c02bd8b1c
VitalyiKudo <vitalyipasechnyi@gmail.com>
2022-01-14 13:45:51 +0200

    add matrix length validation

--- secondTask.js
+++ secondTask.js
@@ -1,27 +1,31 @@
 const matrixValidation = (matrix) => {
   const numberArr = [];
 
-  // merge sections into one array
-  for (let i = 0; i < matrix[0].length - 2; i++) {
-    const filtered = matrix.map((array) =>
-      array.filter((_, index) => index >= i && index < i + 3)
-    );
-    numberArr.push(filtered.flat());
-  }
-
-  // validate sections
-  const result = numberArr.map((arr) =>
-    Boolean(
-      arr.includes(1) &&
+  if(matrix.length < 3) {
+    return "Wrong matrix value"
+  } else {
+    // merge sections into one array
+    for (let i = 0; i < matrix[0].length - 2; i++) {
+      const filtered = matrix.map((array) =>
+        array.filter((_, index) => index >= i && index < i + 3)
+      );
+      numberArr.push(filtered.flat());
+    }
+  
+    // validate sections
+    const result = numberArr.map(
+      (arr) =>
+        arr.includes(1) &&
         arr.includes(2) &&
         arr.includes(3) &&
         arr.includes(4) &&
         arr.includes(5) &&
         arr.includes(6) &&
         arr.includes(7) &&
-        arr.includes(8) & arr.includes(9)
-    )
-  );
-
-  return result;
+        arr.includes(8) &&
+        arr.includes(9)
+    );
+  
+    return result;
+  }
 };
