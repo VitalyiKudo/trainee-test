b1802bd9172e793f79d771ffe81c4a8152195ccf
VitalyiKudo <vitalyipasechnyi@gmail.com>
2022-01-14 13:49:26 +0200

    delete if else

--- fistTask.js
+++ fistTask.js
@@ -1,18 +1,13 @@
 const arrayValidation = (array) => {
-    if (array.length >= 3) {
-      const result = [];
-      for (let i = 1; i < array.length - 1; i++) {
-        if (
-          (array[i] > array[i - 1] && array[i] > array[i + 1]) ||
+  if (array.length >= 3) {
+    const result = [];
+    for (let i = 1; i < array.length - 1; i++) {
+      result.push(Number((
+        (array[i] > array[i - 1] && array[i] > array[i + 1]) ||
           (array[i] < array[i - 1] && array[i] < array[i + 1])
-        ) {
-          result.push(1);
-        } else {
-          result.push(0);
-        }
-      }
-      return result;
+      )));
     }
-  };
+    return result;
+  }
+};
 
-  console.log(arrayValidation([2,5,7,8,1,2]))
